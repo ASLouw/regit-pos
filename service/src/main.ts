dbceb01ebabd792396639ad37915f1c4e53f6318
ASLouw <u15048366@tuks.co.za>
2022-06-04 16:13:37 +0200

    ♻️ (service) Added Service service

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { Transport } from "@nestjs/microservices";
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
+
+const logger = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: "127.0.0.1",
+      port: 8888
+    }
+  });
+  logger.log("Microservice Service is listening on 127.0.0.1:8888")
+  app.listen();
 }
 bootstrap();
