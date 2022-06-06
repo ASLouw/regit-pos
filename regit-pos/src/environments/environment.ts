1889324d99c427e8d67eb434d559989f24083acc
ASLouw <u15048366@tuks.co.za>
2022-06-06 23:22:45 +0200

    ✨(client) Added firebase

--- regit-pos/src/environments/environment.ts
+++ regit-pos/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false.valueOf,
+  firebaseConfig : {
+    apiKey: "AIzaSyCQHlLkpbbcLmy1FcjvUBrfwtJH6CBFqg4",
+    authDomain: "regit-pos.firebaseapp.com",
+    projectId: "regit-pos",
+    storageBucket: "regit-pos.appspot.com",
+    messagingSenderId: "335083985936",
+    appId: "1:335083985936:web:645e39d80ee6f1879dfe95"
+  }
 };
 
 /*
