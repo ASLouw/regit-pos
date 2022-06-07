e4d6a3e7ceecde69133b6c874f07cfd5406b0fb5
ASLouw <u15048366@tuks.co.za>
2022-06-07 23:23:24 +0200

    ✅ (tests): Added tests

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
@@ -13,17 +13,7 @@ async function bootstrap() {
       port: 8888,
     },
   });
-  logger.log('Microservice Service is listening on 0.0.0.0:8080');
+  logger.log('Microservice Service is listening on 127.0.0.1:8888');
   app.listen();
-
-  // const app = await NestFactory.create(AppModule);
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.TCP,
-  //   options: { retryAttempts: 5, retryDelay: 3000 },
-  // });
-
-  // await app.startAllMicroservices();
-  // await app.listen(8888);
-  // console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
